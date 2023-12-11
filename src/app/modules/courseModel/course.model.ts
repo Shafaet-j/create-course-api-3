66f2c2e38ea123c29bc3fc0db39bfa3de830713c
Shafaet Hossen <lbshaft01@gmail.com>
2023-12-11 19:52:34 +0600

    course model done

--- src/app/modules/courseModel/course.model.ts
+++ src/app/modules/courseModel/course.model.ts
@@ -1,4 +1,27 @@
-import { Schema } from "mongoose";
-import { TCourse } from "./course.interface";
+import { Schema, Types, model } from "mongoose";
+import { TCourse, TDetails, TTags } from "./course.interface";
 
-const coureSchema = new Schema<TCourse>({});
+const tagSchema = new Schema<TTags>({
+  name: { type: String, required: true },
+  isDeleted: { type: Boolean, required: true },
+});
+
+const detailsSchema = new Schema<TDetails>({
+  level: { type: String, required: true },
+  description: { type: String, required: true },
+});
+const coureSchema = new Schema<TCourse>({
+  title: { type: String, required: true, unique: true },
+  instructor: { type: String, required: true },
+  categoryId: { type: Schema.Types.ObjectId, required: true, ref: "category" },
+  price: { type: Number, required: true },
+  tags: { type: [tagSchema], required: true },
+  startDate: { type: String, required: true },
+  endDate: { type: String, required: true },
+  language: { type: String, required: true },
+  provider: { type: String, required: true },
+  durationInWeeks: { type: Number, required: true },
+  details: { type: detailsSchema, required: true },
+});
+
+export const Course = model<TCourse>("Course", coureSchema);
